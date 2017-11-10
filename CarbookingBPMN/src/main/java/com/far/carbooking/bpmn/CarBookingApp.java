6acd5393962fae5876a115b21ccff4fd19622db3
13514104 <fairuz.pratama@tokopedia.com>
2017-11-10 17:52:19 +0700

    Added CarbookingBPMN Project

---
+++ CarbookingBPMN/src/main/java/com/far/carbooking/bpmn/CarBookingApp.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.far.carbooking.bpmn;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Car Booking App")
+public class CarBookingApp extends ServletProcessApplication {
+    
+}
